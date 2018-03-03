cffe0eaedb2738f6c6da1116721f8bac5c088396
suyog <suyog@vayu.com.au>
2018-03-03 23:16:41 +0545

    small fix

--- config/prod.exs
+++ config/prod.exs
@@ -66,7 +66,7 @@ config :logger, level: :info
 # import_config "prod.secret.exs"
 
 
-config :chat, ChatWeb.Repo,
+config :chat, Chat.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
